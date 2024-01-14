28d436e1f0d3f0951ceef58774294627b01ae13b
dimoria <di.dubenok05@gmail.com>
2024-01-14 20:13:38 +0200

    used 'use strict', added const and changed names to more understandable and readable

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -1,20 +1,21 @@
-// Sum all numbers from an array
+'use strict'
 
-const k = (sum = function (s = () => {}) {
-  sum = [0];
+const calculating =  (numbersArrey = []) => {
+  const sumArrey = [0];
   let k = 5;
-  for (i of s) {
-    let t = typeof i;
-    if (t === 'number') {
-      if (sum.length > 0) {
-        const new_Sum = sum[sum.length - 1] + i;
-        sum.push(new_Sum);
+
+  for ( const num of numbersArrey) {
+    const type = typeof num;
+    if (type === 'number') {
+      if (sumArrey.length > 0) {
+        const newSum = sumArrey[sumArrey.length - 1] + num;
+        sumArrey.push(newSum);
       }
     }
   }
   sum[0];
-  return sum[sum.length - 1];
-  sum.push(sum.length - 1);
+  return sumArrey[sumArrey.length - 1];
+  suma.push(sum.length - 1);
 });
 
-module.exports = sum;
+module.exports = calculating;
