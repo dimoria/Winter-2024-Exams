481a02e35c1f0350391c6d1b65eb92c7b2233937
dimoria <di.dubenok05@gmail.com>
2024-01-14 19:52:44 +0200

    change code to more small and easier by using '.filter'

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -1,15 +1,8 @@
 'use strict'
 
-const filter = (array, row) => {
-   const arrayWithRemovedElements = [];
-  for ( let elements of array) {
-    let currentIndex = array.indexOf(elements);
-    if (typeof array[currentIndex] !== row) {
-      arrayWithRemovedElements.unshift(currentIndex);
-    }
-  }
-  for (let removedElements of arrayWithRemovedElements) array.splice(removedElements, 1);
-  return array;
+const filter = (array, neededType) => {
+const arrayFiltered = array.filter(element => typeof element === neededType);
+  return arrayFiltered;
 };
 
 module.exports = filter;
