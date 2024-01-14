2a17079b73c621e1f4354af183e8d5bccd0355b0
dimoria <di.dubenok05@gmail.com>
2024-01-14 20:41:20 +0200

    used filter method to create a new array without changing original, rid of frequent using (x), used arrow function syntax

--- Tasks/skip.js
+++ Tasks/skip.js
@@ -1,16 +1,8 @@
 'use strict'
 
  const skipElements = (originalArray, ...elementsToRemove) => {
-  x = 0;
-  for (C of  originalArray) {
-    for (X of elementsToRemove) {
-      if (C === X) {
-        originalArray.splice(x, 1);
-      }
-    }
-    x++;
-  }
-  return originalArray;
+  const newArray = originalArray.filter(item => !elementsToRemove.includes(item));
+  return newArray;
 };
 
 module.exports = skipElements;
