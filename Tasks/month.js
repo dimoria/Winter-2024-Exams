951c539989384e62cd81c2a2ba0401178a9abc8c
between1.js <di.dubenok05@gmail.com>
2024-01-14 17:54:31 +0200

    changed l to 'month.length' for better readability,created a constant for the lowercase input to avoid repeated conversions, used function syntax

--- Tasks/month.js
+++ Tasks/month.js
@@ -2,12 +2,13 @@
 
 const months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec',];
 
- const getMonthsNumber = (s) => {
-  l = months.length;
-  for (let i = 0; i < l; i++) {
-    if (s.toLowerCase().startsWith(months[i])) return i + 1;
+ const getMonthsNumber = (input) => {
+  const lowerCaseInput = input.toLowerCase();
+  for (let i = 0; i < months.length; i++) {
+    if (lowerCaseInput.startsWith(months[i])) {
+     return i + 1;
+    }
   }
   return -1;
 };
-
 module.exports = getMonthsNumber;
