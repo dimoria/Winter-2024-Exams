594202d0aeac623f5cb62c227027a21202b5ef26
between1.js <di.dubenok05@gmail.com>
2024-01-14 16:26:13 +0200

    used 'use strict', add const, made more readable function names, rid of magic-num

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,15 +1,16 @@
 // Get day number
+'use strict'
 
-let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-_parse_day_ = (s) => {
-  let i;
-  for (i = 0; i < D.length; i++) {
-    if (s.startsWith(D[i].toLowerCase())) {
+const parseDay = (input) => {
+  const lowerCaseInput = input.toLowerCase();
+  for (let i = 0; i < daysOfWeek.length; i++) {
+    if (lowerCaseInput.startsWith(daysOfWeek[i].toLowerCase())) {
       return i + 1;
     }
   }
   return -1;
 };
 
-module.exports = _parse_day_;
+module.exports = parseDay;
